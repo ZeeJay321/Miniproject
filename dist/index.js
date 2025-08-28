6c67f8827c87520bb6a7dbb067436c06fc805c85
ZeeJay321 <zain.ul.abidin@qbatch.com>
2025-08-28 16:00:06 +0500

    CLI Added

--- dist/index.js
+++ dist/index.js
@@ -1,18 +1,53 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const moment_1 = __importDefault(require("moment"));
+const readline = __importStar(require("readline"));
 ;
 function addToDoList(todoArray, newTask) {
     todoArray.push(newTask);
-    console.log(`\nNew Task (${newTask}) pushed successfully`);
+    console.log(`\n\nNew Task (${newTask}) pushed successfully`);
 }
 ;
 function printToDoList(todoArray) {
+    console.log('\n\nYour Todo list for the day is as follows:');
     todoArray.forEach(task => {
-        console.log(`\nTask ID ${task.id} | Task Name ${task.name} | Task Creation Time ${task.createdAt} | Task Status ${task.completed}`);
+        console.log(`Task ID ${task.id} | Task Name ${task.name} | Task Creation Time ${task.createdAt} | Task Status ${task.completed}`);
     });
 }
 ;
@@ -23,43 +58,68 @@ function markToDoList(todoArray, id) {
         id: -1,
         completed: false
     };
-    if (markTask.id !== 1 && markTask.completed === false) {
+    if (markTask.id !== -1 && markTask.completed === false) {
         markTask.completed = true;
-        console.log(`\nTask ID ${markTask.id} completed successfully\n`);
+        console.log(`\n\nTask ID ${markTask.id} completed successfully\n`);
     }
     else if (markTask && markTask.completed === true) {
-        console.log(`\nTask ID ${markTask.id} already completed successfully\n`);
+        console.log(`\n\nTask ID ${markTask.id} already completed successfully\n`);
     }
     else {
-        console.log(`\nTask ID ${id} does not exist\n`);
+        console.log(`\n\nTask ID ${id} does not exist\n`);
     }
 }
 ;
-const todoList = [{
-        id: 1,
-        name: 'Learn JavaScript',
-        createdAt: (0, moment_1.default)().format('LLL'),
-        completed: false
-    }, {
-        id: 2,
-        name: 'Learn TypeScript',
-        createdAt: (0, moment_1.default)().format('LLL'),
-        completed: false
-    }, {
-        id: 3,
-        name: 'Build To-Do App',
-        createdAt: (0, moment_1.default)().format('LLL'),
-        completed: false
-    }];
-printToDoList(todoList);
-markToDoList(todoList, 2);
-markToDoList(todoList, 2);
-markToDoList(todoList, 10);
-printToDoList(todoList);
-addToDoList(todoList, {
-    id: 4,
-    name: 'Practice OOP in TS',
-    createdAt: (0, moment_1.default)().format('LLL'),
-    completed: false
+const todoList = [];
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
 });
-printToDoList(todoList);
+function showMenu() {
+    console.log(`\n\n
+=============================
+=         Todo Menu         =
+=============================
+= 1. Print Todo List        =
+= 2. Add a New Task         =
+= 3. Mark Task as Done      =
+= 4. Exit                   =
+=============================
+`);
+    rl.question('\n\nChoose an option (1-4): ', handleMenu);
+}
+function handleMenu(option) {
+    switch (option.trim()) {
+        case "1":
+            printToDoList(todoList);
+            showMenu();
+            break;
+        case "2":
+            rl.question("\nEnter Task Name: ", (taskName) => {
+                const newTask = {
+                    id: todoList.length + 1,
+                    name: taskName,
+                    createdAt: (0, moment_1.default)().format("LLL"),
+                    completed: false
+                };
+                addToDoList(todoList, newTask);
+                showMenu();
+            });
+            break;
+        case "3":
+            rl.question("\nEnter Task ID to mark as done: ", (taskId) => {
+                markToDoList(todoList, parseInt(taskId));
+                showMenu();
+            });
+            break;
+        case "4":
+            console.log("\nExiting Todo App. Goodbye!");
+            rl.close();
+            break;
+        default:
+            console.log("\nInvalid option, please try again");
+            showMenu();
+            break;
+    }
+}
+showMenu();
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import moment from "moment";
+import * as readline from "readline";
 
 interface Todo {
   id: number,
@@ -9,12 +10,13 @@ interface Todo {
 
 function addToDoList(todoArray: Todo[], newTask: Todo): void {
   todoArray.push(newTask);
-  console.log(`\nNew Task (${newTask}) pushed successfully`);
+  console.log(`\n\nNew Task (${newTask}) pushed successfully`);
 };
 
 function printToDoList(todoArray: Todo[]): void {
+  console.log('\n\nYour Todo list for the day is as follows:');
   todoArray.forEach(task => {
-    console.log(`\nTask ID ${task.id} | Task Name ${task.name} | Task Creation Time ${task.createdAt} | Task Status ${task.completed}`);
+    console.log(`Task ID ${task.id} | Task Name ${task.name} | Task Creation Time ${task.createdAt} | Task Status ${task.completed}`);
   });
 };
 
@@ -26,48 +28,74 @@ function markToDoList(todoArray: Todo[], id: number): void {
     completed: false
   };
 
-  if (markTask.id !== 1 && markTask.completed === false) {
+  if (markTask.id !== -1 && markTask.completed === false) {
     markTask.completed = true;
-    console.log(`\nTask ID ${markTask.id} completed successfully\n`);
+    console.log(`\n\nTask ID ${markTask.id} completed successfully\n`);
   } else if (markTask && markTask.completed === true) {
-    console.log(`\nTask ID ${markTask.id} already completed successfully\n`);
+    console.log(`\n\nTask ID ${markTask.id} already completed successfully\n`);
   } else {
-    console.log(`\nTask ID ${id} does not exist\n`);
+    console.log(`\n\nTask ID ${id} does not exist\n`);
   }
 };
 
-const todoList: Todo[] = [{ 
-  id: 1,
-  name: 'Learn JavaScript',
-  createdAt: moment().format('LLL'),
-  completed: false 
-}, { 
-  id: 2,
-  name: 'Learn TypeScript',
-  createdAt: moment().format('LLL'),
-  completed: false 
-}, { 
-  id: 3,
-  name: 'Build To-Do App',
-  createdAt:moment().format('LLL'),
-  completed: false 
-}];
+const todoList: Todo[] = [];
 
-printToDoList(todoList);
-
-markToDoList(todoList, 2);
-
-markToDoList(todoList, 2);
-
-markToDoList(todoList, 10);
-
-printToDoList(todoList);
-
-addToDoList(todoList, {
-  id: 4,
-  name: 'Practice OOP in TS',
-  createdAt: moment().format('LLL'),
-  completed: false
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
 });
 
-printToDoList(todoList);
+function showMenu() {
+  console.log(`\n\n
+=============================
+=         Todo Menu         =
+=============================
+= 1. Print Todo List        =
+= 2. Add a New Task         =
+= 3. Mark Task as Done      =
+= 4. Exit                   =
+=============================
+`);
+  rl.question('\n\nChoose an option (1-4): ', handleMenu);
+}
+
+function handleMenu(option: string) {
+  switch (option.trim()) {
+    case "1":
+      printToDoList(todoList);
+      showMenu();
+      break;
+
+    case "2":
+      rl.question("\nEnter Task Name: ", (taskName: string) => {
+        const newTask: Todo = {
+          id: todoList.length + 1,
+          name: taskName,
+          createdAt: moment().format("LLL"),
+          completed: false
+        };
+        addToDoList(todoList, newTask);
+        showMenu();
+      });
+      break;
+
+    case "3":
+      rl.question("\nEnter Task ID to mark as done: ", (taskId: string) => {
+        markToDoList(todoList, parseInt(taskId));
+        showMenu();
+      });
+      break;
+
+    case "4":
+      console.log("\nExiting Todo App. Goodbye!");
+      rl.close();
+      break;
+
+    default:
+      console.log("\nInvalid option, please try again");
+      showMenu();
+      break;
+  }
+}
+
+showMenu();
